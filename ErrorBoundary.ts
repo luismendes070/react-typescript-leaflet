bb6ffe77f6430a3d54e7b27bb6633c0a5df907c1
luismendes070 <machadoluismendes@gmail.com>
2023-03-14 21:12:18 -0300

    1 file modified

--- ErrorBoundary.ts
+++ ErrorBoundary.ts
@@ -1,17 +1,21 @@
 import React from "react";
 
-export default class ErrorBoundary extends React.Component {
-  constructor(props:any) {
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends React.Component<{}, ErrorBoundaryState> {
+  constructor(props: {}) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error:any) {
+  static getDerivedStateFromError(error: any) {
     // Atualiza o state para que a próxima renderização mostre a UI alternativa.
     return { hasError: true };
   }
 
-  componentDidCatch(error:any, errorInfo:any) {
+  componentDidCatch(error: any, errorInfo: any) {
     // Você também pode registrar o erro em um serviço de relatórios de erro
     logErrorToMyService(error, errorInfo);
   }
@@ -19,13 +23,13 @@ export default class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // Você pode renderizar qualquer UI alternativa
-      return ${{"<h1>Algo deu errado.</h1>"}};
+      return <h1>Algo deu errado.</h1>;
     }
 
     return this.props.children; 
   }
 }
 
-function logErrorToMyService(error: any,errorInfo: any) {
-throw new Error("Function not implemented.");
+function logErrorToMyService(error: any, errorInfo: any) {
+  throw new Error("Function not implemented.");
 }
